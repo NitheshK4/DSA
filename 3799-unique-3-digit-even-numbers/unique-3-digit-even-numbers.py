b40e543ee5a4be2c910f947d39e61fb58d6ceaec
Nithesh kumar <nitheshk236@gmail.com>
2026-09-11 09:40:40 +0530

    Time: 19 ms (55.38%) | Memory: 19.1 MB (92.09%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        s=set()
+        n=len(digits)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    if i!=j and j!=k and i!=k:
+                        if digits[i]!=0 and digits[k]%2==0:
+                            s.add(digits[i]*100+digits[j]*10+digits[k])
+        return len(s)
